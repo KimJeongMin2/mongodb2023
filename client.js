de0a8054089107a8bac2db546f016d23aad5ca43
kimjeongmin <kij9451@naver.com>
2023-12-04 22:14:04 +0900

    숙소 상세 조회에 리뷰 추가, 달력 올바르게 출력 되도록 수정

--- client.js
+++ client.js
@@ -1,48 +1,44 @@
 const axios = require("axios");
 
-axios.post('http://127.0.0.1:3000/test', {
-    name: 'test name',
-    description: 'test description'
-})
-.then(function (response) {
+axios.post('http://127.0.0.1:3000/lodging/generate')
+  .then(function (response) {
     console.log(response.data);
-})
-.catch(function (error) {
+  })
+  .catch(function (error) {
     console.log(error);
-});
+  });
 
-axios.get('http://127.0.0.1:3000/lodging/search', {
-  params: {
-    checkIn: '2023-11-28',
-    checkOut: '2023-11-30',
-    guests: 2,
-    lodgingType: '개인실'
-  }
-})
-.then(response => {
-  console.log(response.data);
-})
-.catch(error => {
-  console.error(error);
-});
 
-axios.get(`http://127.0.0.1:3000/lodging/${id}`, {
-  params: {
-    month: month
-  }
-})
-.then(response => {
-  const data = response.data;
+// axios.get('http://127.0.0.1:3000/lodging/search', {
+//   params: {
+//     checkIn: '2023-11-28',
+//     checkOut: '2023-11-30',
+//     guests: 2,
+//     lodgingType: '개인실'
+//   }
+// })
+// .then(response => {
+//   console.log(response.data);
+// })
+// .catch(error => {
+//   console.error(error);
+// });
 
-  console.log('숙소 기본 정보:', data.lodging);
-  console.log('모든 별점과 리뷰:', data.reviews);
-  console.log('예약 현황(달력 형태):', data.calendar.join(' '));
-  if (data.availableRooms !== undefined) {
-    console.log('예약 가능한 방의 개수:', data.availableRooms);
-  }
-})
-.catch(error => {
-  console.error('숙소의 상세 정보를 조회하는 중에 오류가 발생했습니다.', error);
-});
+// axios.get(`http://127.0.0.1:3000/lodging/656b01f69304f859f22e0e1f`, {
+//   params: {
+//     month: "2023-11"
+//   }
+// })
+// .then(response => {
+//   const data = response.data;
+  
+//   console.log("숙소 정보:\n",data.lodging)
+//   console.log(data.reviews.map(review => `Review: ${review.review}, Rating: ${review.rating}`));
+//   console.log(data.calendar);
+
+// })
+// .catch(error => {
+//   console.error('숙소의 상세 정보를 조회하는 중에 오류가 발생했습니다.', error);
+// });
 
 
