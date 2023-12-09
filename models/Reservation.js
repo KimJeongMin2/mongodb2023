94ea0cf20d41c701aeed74fa8b5b23f8ea5c1d4a
Junhyun <dole614@naver.com>
2023-12-10 00:00:00 +0900

    숙소 예약 등록, 예약 취소, 게스트 생성 기능 구현 완료

--- models/Reservation.js
+++ models/Reservation.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const reservationSchema = new mongoose.Schema({
   checkIn: Date,
   checkOut: Date,
   guests: Number,
   totalFee: Number,
-  lodging: { type: mongoose.Schema.Types.ObjectId, ref: 'Lodging' },
-  memberId: { type: mongoose.Schema.Types.ObjectId, ref: 'Member' },
-  reviewId: { type: mongoose.Schema.Types.ObjectId, ref: 'Review' }
+  lodging: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
+    ref: "Lodging",
+  },
+  guest: { type: mongoose.Schema.Types.ObjectId, ref: "Guest" },
+  review: { type: mongoose.Schema.Types.ObjectId, ref: "Review" },
 });
 
-module.exports = mongoose.model('Reservation', reservationSchema);
+module.exports = mongoose.model("Reservation", reservationSchema);
